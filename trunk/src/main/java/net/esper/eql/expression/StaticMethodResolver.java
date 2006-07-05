dbffbd1003b164b25169a96b1ac8bf72b49016f3
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-07-05 21:24:29 +0000

    Now is only static and doesn't offer the user-specified import functionality.
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/staticmethods@59 d91eb1b3-1215-0410-9718-d41490442091

--- trunk/src/main/java/net/esper/eql/expression/StaticMethodResolver.java
+++ trunk/src/main/java/net/esper/eql/expression/StaticMethodResolver.java
@@ -10,125 +10,104 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
- * A helper class used for retrieving static methods objects. It
+ * Used for retrieving static method objects. It
  * provides two points of added functionality over the standard 
  * java.lang.reflect mechanism of retrieving methods. First, 
- * class names can be partial, and a user-specified list of 
- * package names can be used to search for the class. Second, 
+ * class names can be partial, and if the class name is partial
+ * then java.lang is searched for the class. Second, 
  * invocation parameter types don't have to match the declaration
  * parameter types exactly when the standard java conversion
- * mechanisms (autoboxing, widening conversions etc.) will make 
- * the invocation valid. Preference is given to those methods that
- * require the fewest parameter conversions.
+ * mechanisms (currently autoboxing and widening conversions) 
+ * will make the invocation valid. Preference is given to those 
+ * methods that require the fewest widening conversions.
  */
 public class StaticMethodResolver 
 {
-	private final List<String> classPath = new ArrayList<String>();
-	private static final Map<Class, Set<Class>> validTypeConversions = new HashMap<Class, Set<Class>>();
+	private static final List<String> imports = new ArrayList<String>();
+	private static final Map<Class, Set<Class>> wideningConversions = new HashMap<Class, Set<Class>>();
+	private static final Map<Class, Set<Class>> wrappingConversions = new HashMap<Class, Set<Class>>();
 	
-	// Initialize the map of valid parameter type conversions
+	 private static final Log log = LogFactory.getLog(StaticMethodResolver.class);
+	 
 	static 
 	{
-		Set<Class> booleanTypes = new HashSet<Class>();
-		booleanTypes.add(boolean.class);
-		booleanTypes.add(Boolean.class);
-		StaticMethodResolver.validTypeConversions.put(boolean.class, booleanTypes);
-		StaticMethodResolver.validTypeConversions.put(Boolean.class, booleanTypes);
+		// Initialize the imports
+		imports.add("java.lang");
 		
-		Set<Class> charTypes = new HashSet<Class>();
-		charTypes.add(char.class);
-		charTypes.add(Character.class);		
-		StaticMethodResolver.validTypeConversions.put(char.class, charTypes);
-		StaticMethodResolver.validTypeConversions.put(Character.class, charTypes);
+		// Initialize the map of wrapper conversions
+		Set<Class> booleanWrappers = new HashSet<Class>();
+		booleanWrappers.add(boolean.class);
+		booleanWrappers.add(Boolean.class);
+		StaticMethodResolver.wrappingConversions.put(boolean.class, booleanWrappers);
+		StaticMethodResolver.wrappingConversions.put(Boolean.class, booleanWrappers);
 		
-		Set<Class> byteTypes = new HashSet<Class>();
-		byteTypes.add(byte.class);
-		byteTypes.add(Byte.class);
-		StaticMethodResolver.validTypeConversions.put(byte.class, byteTypes);
-		StaticMethodResolver.validTypeConversions.put(Byte.class, byteTypes);
+		Set<Class> charWrappers = new HashSet<Class>();
+		charWrappers.add(char.class);
+		charWrappers.add(Character.class);		
+		StaticMethodResolver.wrappingConversions.put(char.class, charWrappers);
+		StaticMethodResolver.wrappingConversions.put(Character.class, charWrappers);
 		
-		Set<Class> shortTypes = new HashSet<Class>();
-		shortTypes.add(short.class);
-		shortTypes.add(Short.class);
-		shortTypes.addAll(byteTypes);
-		StaticMethodResolver.validTypeConversions.put(short.class, shortTypes);
-		StaticMethodResolver.validTypeConversions.put(Short.class, shortTypes);
-	
-		Set<Class> intTypes = new HashSet<Class>();
-		intTypes.add(int.class);
-		intTypes.add(Integer.class);
-		intTypes.addAll(shortTypes);
-		StaticMethodResolver.validTypeConversions.put(int.class, intTypes);
-		StaticMethodResolver.validTypeConversions.put(Integer.class, intTypes);
+		Set<Class> byteWrappers = new HashSet<Class>();
+		byteWrappers.add(byte.class);
+		byteWrappers.add(Byte.class);
+		StaticMethodResolver.wrappingConversions.put(byte.class, byteWrappers);
+		StaticMethodResolver.wrappingConversions.put(Byte.class, byteWrappers);
 		
-		Set<Class> longTypes = new HashSet<Class>();
-		longTypes.add(long.class);
-		longTypes.add(Long.class);
-		longTypes.addAll(intTypes);
-		StaticMethodResolver.validTypeConversions.put(long.class, longTypes);
-		StaticMethodResolver.validTypeConversions.put(Long.class, longTypes);
+		Set<Class> shortWrappers = new HashSet<Class>();
+		shortWrappers.add(short.class);
+		shortWrappers.add(Short.class);
+		StaticMethodResolver.wrappingConversions.put(short.class, shortWrappers);
+		StaticMethodResolver.wrappingConversions.put(Short.class, shortWrappers);
 		
-		Set<Class> floatTypes = new HashSet<Class>();
-		floatTypes.add(float.class);
-		floatTypes.add(Float.class);
-		floatTypes.addAll(longTypes);
-		StaticMethodResolver.validTypeConversions.put(float.class, floatTypes);
-		StaticMethodResolver.validTypeConversions.put(Float.class, floatTypes);
+		Set<Class> intWrappers = new HashSet<Class>();
+		intWrappers.add(int.class);
+		intWrappers.add(Integer.class);
+		StaticMethodResolver.wrappingConversions.put(int.class, intWrappers);
+		StaticMethodResolver.wrappingConversions.put(Integer.class, intWrappers);
 		
-		Set<Class> doubleTypes = new HashSet<Class>();
-		doubleTypes.add(double.class);
-		doubleTypes.add(Double.class);
-		doubleTypes.addAll(floatTypes);
-		StaticMethodResolver.validTypeConversions.put(double.class, doubleTypes);
-		StaticMethodResolver.validTypeConversions.put(Double.class, doubleTypes);
-	}
+		Set<Class> longWrappers = new HashSet<Class>();
+		longWrappers.add(long.class);
+		longWrappers.add(Long.class);
+		StaticMethodResolver.wrappingConversions.put(long.class, longWrappers);
+		StaticMethodResolver.wrappingConversions.put(Long.class, longWrappers);
+		
+		Set<Class> floatWrappers = new HashSet<Class>();
+		floatWrappers.add(float.class);
+		floatWrappers.add(Float.class);
+		StaticMethodResolver.wrappingConversions.put(float.class, floatWrappers);
+		StaticMethodResolver.wrappingConversions.put(Float.class, floatWrappers);
+		
+		Set<Class> doubleWrappers = new HashSet<Class>();
+		doubleWrappers.add(double.class);
+		doubleWrappers.add(Double.class);
+		StaticMethodResolver.wrappingConversions.put(double.class, doubleWrappers);
+		StaticMethodResolver.wrappingConversions.put(Double.class, doubleWrappers);
 
-	/**
-	 * Ctor.
-	 * @param classPath - the names of the library packages that 
-	 * will be searched to try to resolve an incomplete class name
-	 */
-	public StaticMethodResolver(String[] classPath)
-	{
-		if(classPath == null)
-		{
-			throw new NullPointerException();
-		}
-		this.classPath.addAll(Arrays.asList(classPath));
-	}
+		// Initialize the map of widening conversions
+		Set<Class> wideningConversions = new HashSet<Class>(byteWrappers);
+		StaticMethodResolver.wideningConversions.put(short.class, new HashSet<Class>(wideningConversions));
+		StaticMethodResolver.wideningConversions.put(Short.class, new HashSet<Class>(wideningConversions));
+		
+		wideningConversions.addAll(shortWrappers);
+		wideningConversions.addAll(charWrappers);
+		StaticMethodResolver.wideningConversions.put(int.class, new HashSet<Class>(wideningConversions));
+		StaticMethodResolver.wideningConversions.put(Integer.class, new HashSet<Class>(wideningConversions));
+		
+		wideningConversions.addAll(intWrappers);
+		StaticMethodResolver.wideningConversions.put(long.class, new HashSet<Class>(wideningConversions));
+		StaticMethodResolver.wideningConversions.put(Long.class, new HashSet<Class>(wideningConversions));
+
+		wideningConversions.addAll(longWrappers);
+		StaticMethodResolver.wideningConversions.put(float.class, new HashSet<Class>(wideningConversions));
+		StaticMethodResolver.wideningConversions.put(Float.class, new HashSet<Class>(wideningConversions));
 	
-	/**
-	 * Ctor.
-	 * The classpath for resolving partial class names 
-	 * will be initialized with "java.lang"
-	 */
-	public StaticMethodResolver()
-	{
-		this.classPath.add("java.lang");
-	}
-	
-	/**
-	 * @return the names of the library packages that will be
-	 * searched to try to resolve an incomplete class name
-	 */
-	public String[] getClassPath() 
-	{
-		return classPath.toArray(new String[0]);
-	}
-	
-	/**
-	 * @param classPath - the names of the library packages that 
-	 * will be searched to try to resolve an incomplete class name
-	 */
-	public void setClassPath(String[] classPath)
-	{
-		if(classPath == null)
-		{
-			throw new NullPointerException();
-		}
-		this.classPath.clear();
-		this.classPath.addAll(Arrays.asList(classPath));
+		wideningConversions.addAll(floatWrappers);
+		StaticMethodResolver.wideningConversions.put(double.class, new HashSet<Class>(wideningConversions));
+		StaticMethodResolver.wideningConversions.put(Double.class, new HashSet<Class>(wideningConversions));
 	}
 	
 	/**
@@ -142,9 +121,10 @@ public class StaticMethodResolver
 	 * @throws ClassNotFoundException
 	 * @throws NoSuchMethodException
 	 */
-	public Method resolveMethod(String className, String methodName, Class[] paramTypes)
+	public static Method resolveMethod(String className, String methodName, Class[] paramTypes)
 	throws ClassNotFoundException, NoSuchMethodException
 	{
+		log.debug(".resolve method className==" + className + ", methodName==" + methodName);
 		// Get the declaring class
 		Class declaringClass = resolveClass(className);
 		
@@ -203,26 +183,36 @@ public class StaticMethodResolver
 			}
 				
 		}
-		
+
 		if(bestMatch != null)
 		{
 			return bestMatch;
 		}
 		else
 		{
-			throw new NoSuchMethodException("Unknown method " + className + "." + methodName);
+			StringBuffer params = new StringBuffer();
+			if(paramTypes != null && paramTypes.length != 0)
+			{
+				String appendString = "";
+				for(Object param : paramTypes)
+				{
+					params.append(appendString);
+					params.append(param.toString());
+					appendString = ", ";
+				}
+			}
+			throw new NoSuchMethodException("Unknown method " + className + "." + methodName + "(" + params + ")");
 		}
 	}
 	
 	/**
 	 * Gets the Class object for the class name. If the class 
-	 * name is incomplete, uses the values in the classpath
-	 * to search specific packages for this class.
+	 * name is incomplete, searches java.lang
 	 * @param className - the name of the class to resolve
 	 * @return - the Class object for this class
 	 * @throws ClassNotFoundException
 	 */
-	protected Class resolveClass(String className) 
+	protected static Class resolveClass(String className) 
 	throws ClassNotFoundException
 	{
 		Class result = null;
@@ -237,7 +227,7 @@ public class StaticMethodResolver
 		catch(ClassNotFoundException e){}
 		
 		// Try all the package prefixes in the classpath
-		for(String prefix : classPath)
+		for(String prefix : imports)
 		{
 			String prefixedClassName = prefix + "." + className;
 			try
@@ -252,27 +242,19 @@ public class StaticMethodResolver
 		throw new ClassNotFoundException("Unknown class " + className);
 	}
 
-	/**
-	 * Resolves whether a parameter type in a method invocation
-	 * is valid, possibly after autoboxing and widening conversions.
-	 * @param declarationType - the type in the method declaration
-	 * @param invocationType - the type in the method invocation
-	 * @return true if an object of type invocationType can 
-	 * be used in place of a object of type declarationType
-	 */
-	protected static boolean isValidTypeConversion(Class declarationType, Class invocationType)
+	private static boolean isWideningConversion(Class declarationType, Class invocationType)
 	{
-		if(validTypeConversions.containsKey(declarationType))
+		if(wideningConversions.containsKey(declarationType))
 		{
-			return validTypeConversions.get(declarationType).contains(invocationType);
+			return wideningConversions.get(declarationType).contains(invocationType);
 		}
 		else
 		{
-			return declarationType.isAssignableFrom(invocationType);
+			return false;
 		}
 	}
 	
-	private boolean isPublicAndStatic(Method method)
+	private static boolean isPublicAndStatic(Method method)
 	{
 		int modifiers = method.getModifiers();
 		return Modifier.isPublic(modifiers) && Modifier.isStatic(modifiers);
@@ -280,25 +262,30 @@ public class StaticMethodResolver
 	
 	// Returns -1 if the invocation parameters aren't applicable
 	// to the method. Otherwise returns the number of parameters
-	// that have to be adapted in some way (autoboxing, widening
-	// conversion etc.).
-	private int compareParameterTypes(Method method, Class[] invocationParameters)
+	// that have to be converted
+	private static int compareParameterTypes(Method method, Class[] invocationParameters)
 	{
-		Class[] declarationParameters = method.getParameterTypes();
 
+		Class[] declarationParameters = method.getParameterTypes();
+		
+		if(invocationParameters == null)
+		{
+			return declarationParameters.length == 0 ? 0 : -1;
+		}
+		
 		if(declarationParameters.length != invocationParameters.length)
 		{
 			return -1;
 		}
-		
+
 		int conversionCount = 0;
 		int count = 0;
 		for(Class parameter : declarationParameters)
 		{
-			if(!parameter.equals(invocationParameters[count]))
+			if(!isIdentityConversion(parameter, invocationParameters[count]))
 			{
 				conversionCount++;
-				if(!StaticMethodResolver.isValidTypeConversion(parameter, invocationParameters[count]))
+				if(!isWideningConversion(parameter, invocationParameters[count]))
 				{
 					conversionCount = -1;
 					break;
@@ -306,6 +293,22 @@ public class StaticMethodResolver
 			}
 			count++;
 		}
+
 		return conversionCount;
+	}
+	
+	// Identity conversion means no conversion, wrapper conversion, 
+	// or conversion to a supertype
+	private static boolean isIdentityConversion(Class declarationType, Class invocationType)
+	{
+		if(wrappingConversions.containsKey(declarationType))
+		{
+			return wrappingConversions.get(declarationType).contains(invocationType) || declarationType.isAssignableFrom(invocationType);	
+		}
+		else 
+		{
+			return declarationType.isAssignableFrom(invocationType);
+		}
+
 	}
 }
