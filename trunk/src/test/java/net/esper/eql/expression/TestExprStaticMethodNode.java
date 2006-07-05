180fe1e5ab152cd544636e1073a73f742cea4271
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-07-05 21:29:57 +0000

    Full suite of tests. Removed certain tests for obsolete behavior.
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/staticmethods@65 d91eb1b3-1215-0410-9718-d41490442091

--- trunk/src/test/java/net/esper/eql/expression/TestExprStaticMethodNode.java
+++ trunk/src/test/java/net/esper/eql/expression/TestExprStaticMethodNode.java
@@ -7,15 +7,21 @@ import net.esper.support.eql.SupportStreamTypeSvc1Stream;
 
 public class TestExprStaticMethodNode extends TestCase 
 {
+	StaticMethodResolver staticMethodResolver;
+	StreamTypeService streamTypeService;
 	ExprNode intThree;
 	ExprNode intFive;
 	ExprNode shortNine;
 	ExprNode doubleFour;
 	ExprNode doubleEight;
 	ExprNode stringTen;
+	Method maxInt;
+	Method maxDouble;
 	
 	protected void setUp() throws Exception 
 	{
+		streamTypeService = null;
+		staticMethodResolver = new StaticMethodResolver();
 		intThree = new ExprConstantNode(3);
 		intFive = new ExprConstantNode(5);
 		short nine = 9;
@@ -23,40 +29,45 @@ public class TestExprStaticMethodNode extends TestCase
 		doubleFour = new ExprConstantNode(4d);
 		doubleEight = new ExprConstantNode(8d);
 		stringTen = new ExprConstantNode("10");
+		maxInt = Math.class.getMethod("max", int.class, int.class);
+		maxDouble = Math.class.getMethod("max", double.class, double.class);
 	}
 	
 	public void testMaxIntInt() throws Exception
 	{
-		Method staticMethod = Math.class.getMethod("max", new Class[] { int.class, int.class });
-		ExprNode root = new ExprStaticMethodNode(staticMethod);
+		ExprStaticMethodNode root = new ExprStaticMethodNode("Math", "max");
 		root.addChildNode(intThree);
 		root.addChildNode(intFive);
+		validate(root);	
 		
+		assertEquals(maxInt, root.getStaticMethod());
 		Integer result = Math.max(3,5);
 		assertEquals(result, root.evaluate(null));
 	}
 	
 	public void testIntegerInt() throws Exception
 	{
-		Method parentMethod = this.getClass().getMethod("staticIntMethod", Integer.class);
-		ExprNode parent = new ExprStaticMethodNode(parentMethod);
-		Method childMethod = Math.class.getMethod("max", new Class[] { int.class, int.class });
-		ExprNode child = new ExprStaticMethodNode(childMethod);
+		Method staticMethod = this.getClass().getMethod("staticIntMethod", Integer.class);
+		ExprStaticMethodNode parent = new ExprStaticMethodNode(this.getClass().getName(), "staticIntMethod");
+		ExprNode child = new ExprStaticMethodNode("Math", "max");
 		child.addChildNode(intThree);
 		child.addChildNode(intFive);
 		parent.addChildNode(child);
-
+		validate(parent);	
+		
+		assertEquals(staticMethod, parent.getStaticMethod());
 		int result = Math.max(3, 5);
 		assertEquals(result, parent.evaluate(null));
 	}
 	
 	public void testMaxIntShort() throws Exception
 	{
-		Method staticMethod = Math.class.getMethod("max", new Class[] { int.class, int.class });
-		ExprNode root = new ExprStaticMethodNode(staticMethod);
+		ExprStaticMethodNode root = new ExprStaticMethodNode("Math", "max");
 		root.addChildNode(intThree);
 		root.addChildNode(shortNine);
+		validate(root);
 		
+		assertEquals(maxInt, root.getStaticMethod());
 		short nine = 9;
 		Integer result = Math.max(3,nine);
 		assertEquals(result, root.evaluate(null));
@@ -64,152 +75,56 @@ public class TestExprStaticMethodNode extends TestCase
 	
 	public void testMaxDoubleInt() throws Exception
 	{
-		Method staticMethod = Math.class.getMethod("max", new Class[] { double.class, double.class });
-		ExprNode root = new ExprStaticMethodNode(staticMethod);
+		ExprStaticMethodNode root = new ExprStaticMethodNode("Math", "max");
 		root.addChildNode(doubleEight);
 		root.addChildNode(intFive);
+		validate(root);
 		
+		assertEquals(maxDouble, root.getStaticMethod());
 		Double result = Math.max(8d,5);
 		assertEquals(result, root.evaluate(null));
 	}
 	
 	public void testMaxDoubleDouble() throws Exception
 	{
-		Method staticMethod = Math.class.getMethod("max", new Class[] { double.class, double.class });
-		ExprNode root = new ExprStaticMethodNode(staticMethod);
+		ExprStaticMethodNode root = new ExprStaticMethodNode("Math", "max");
 		root.addChildNode(doubleEight);
 		root.addChildNode(doubleFour);
+		validate(root);
 		
+		assertEquals(maxDouble, root.getStaticMethod());
 		Double result = Math.max(8d,4d);
 		assertEquals(result, root.evaluate(null));
 	}
 	
 	public void testPowDoubleDouble() throws Exception
 	{
-		Method staticMethod = Math.class.getMethod("pow", new Class[] { double.class, double.class });
-		ExprNode root = new ExprStaticMethodNode(staticMethod);
+		Method pow = Math.class.getMethod("pow", double.class, double.class);
+		ExprStaticMethodNode root = new ExprStaticMethodNode("Math", "pow");
 		root.addChildNode(doubleEight);
 		root.addChildNode(doubleFour);
+		validate(root);
 		
+		assertEquals(pow, root.getStaticMethod());
 		Double result = Math.pow(8d,4d);
 		assertEquals(result, root.evaluate(null));
 	}
 	
 	public void testValueOfInt() throws Exception
 	{
-		Method staticMethod = Integer.class.getMethod("valueOf", new Class[] { String.class });
-		ExprNode root = new ExprStaticMethodNode(staticMethod);
+		Method valueOf = Integer.class.getMethod("valueOf", String.class);
+		ExprStaticMethodNode root = new ExprStaticMethodNode("Integer", "valueOf");
 		root.addChildNode(stringTen);
+		validate(root);
 		
+		assertEquals(valueOf, root.getStaticMethod());
 		Integer result = Integer.valueOf("10");
 		assertEquals(result, root.evaluate(null));
 	}
 	
-	public void testConstructor() throws Exception
+	private void validate(ExprNode node) throws Exception
 	{
-		ExprStaticMethodNode root;
-		
-		try 
-		{
-			root = new ExprStaticMethodNode(null);
-			fail();
-		} 
-		catch (NullPointerException e) 
-		{
-			// Expected
-		}
-		
-		try 
-		{
-			root = new ExprStaticMethodNode(this.getClass().getMethod("nonstaticMethod", null));
-			fail();
-		} 
-		catch (IllegalArgumentException e) 
-		{
-			// Expected
-		}
-		
-		root = new ExprStaticMethodNode(this.getClass().getMethod("staticMethod", null));
-		assertEquals(this.getClass().getMethod("staticMethod", null), root.getStaticMethod());
-	}
-	
-	public void testEqualsNode() throws Exception
-	{
-		Method staticMethodOne = Math.class.getMethod("max", new Class[] { int.class, int.class });
-		Method staticMethodTwo = Math.class.getMethod("max", new Class[] { double.class, double.class });		
-		
-		ExprNode rootOne = new ExprStaticMethodNode(staticMethodOne);
-		ExprNode rootTwo = new ExprStaticMethodNode(staticMethodOne);
-		ExprNode rootThree = new ExprStaticMethodNode(staticMethodTwo);
-		ExprNode rootFour = new ExprConstantNode(3d);
-
-		assertTrue(rootOne.equalsNode(rootTwo));
-		assertFalse(rootOne.equalsNode(rootThree));
-		assertFalse(rootOne.equalsNode(rootFour));
-		assertFalse(rootOne.equalsNode(null));
-	}
-	
-	public void testGetType() throws Exception
-	{
-		Method staticMethod = Math.class.getMethod("max", new Class[] { int.class, int.class });
-		ExprNode root = new ExprStaticMethodNode(staticMethod);
-		assertEquals(staticMethod.getReturnType(), root.getType());
-	}
-	
-	public void testValidate() throws Exception
-	{
-		Method staticMethod = Math.class.getMethod("max", new Class[] { int.class, int.class });
-	    StreamTypeService streamTypeService = new SupportStreamTypeSvc1Stream();
-	    
-		ExprNode root = new ExprStaticMethodNode(staticMethod);	    
-		root.addChildNode(intFive);
-	    root.addChildNode(intThree);
-	    root.validate(streamTypeService);
-	    
-	    root = new ExprStaticMethodNode(staticMethod);	    
-		root.addChildNode(intFive);
-	    root.addChildNode(shortNine);
-	    root.validate(streamTypeService);
-	}
-	
-	public void testValidateInvalid() throws Exception
-	{
-		Method staticMethod = Math.class.getMethod("max", new Class[] { int.class, int.class });
-		ExprNode root = new ExprStaticMethodNode(staticMethod);
-	    StreamTypeService streamTypeService = new SupportStreamTypeSvc1Stream();
-
-	    root.addChildNode(intFive);
-	    try
-		{
-			root.validate(streamTypeService);
-			fail();
-		}
-		catch(ExprValidationException e)
-		{
-			// Expected
-		}
-		
-		root.addChildNode(doubleFour);
-	    try
-		{
-			root.validate(streamTypeService);
-			fail();
-		}
-		catch(ExprValidationException e)
-		{
-			// Expected
-		}
-		
-	    root.addChildNode(intFive);
-	    try
-		{
-			root.validate(streamTypeService);
-			fail();
-		}
-		catch(ExprValidationException e)
-		{
-			// Expected
-		}
+		node.getValidatedSubtree(streamTypeService);
 	}
 	
 	public void nonstaticMethod(){}
