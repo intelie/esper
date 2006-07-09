92a5f667045e5120431f5010433b80b842facdff
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-07-09 19:51:58 +0000

    Updated for changes to Configuration.
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/staticmethods@90 d91eb1b3-1215-0410-9718-d41490442091

--- trunk/src/test/java/net/esper/client/TestConfiguration.java
+++ trunk/src/test/java/net/esper/client/TestConfiguration.java
@@ -5,10 +5,12 @@ import junit.framework.TestCase;
 import java.net.URL;
 import java.io.File;
 
+
 public class TestConfiguration extends TestCase
 {
     private final static String SAMPLE_EVENT_NAME = "MySampleEvent";
     private final static String SAMPLE_EVENT_CLASS = "com.mycompany.myapp.MySampleEvent";
+    private final static String SAMPLE_IMPORT_PACKAGE = "com.mycompany.myapp.*";
 
     private Configuration config;
 
@@ -20,20 +22,20 @@ public class TestConfiguration extends TestCase
     public void testDefault() throws Exception
     {
         config.configure();
-        assertConfig(config);
+        assertConfig(config, false);
     }
 
     public void testString() throws Exception
     {
         config.configure(Configuration.ESPER_DEFAULT_CONFIG);
-        assertConfig(config);
+        assertConfig(config, false);
     }
 
     public void testURL() throws Exception
     {
         URL url = this.getClass().getClassLoader().getResource(Configuration.ESPER_DEFAULT_CONFIG);
         config.configure(url);
-        assertConfig(config);
+        assertConfig(config, false);
     }
 
     public void testFile() throws Exception
@@ -41,18 +43,34 @@ public class TestConfiguration extends TestCase
         URL url = this.getClass().getClassLoader().getResource(Configuration.ESPER_DEFAULT_CONFIG);
         File file = new File(url.toURI());
         config.configure(file);
-        assertConfig(config);
+        assertConfig(config, false);
     }
 
     public void testAddEventMapping()
     {
         config.addEventTypeAlias(SAMPLE_EVENT_NAME, SAMPLE_EVENT_CLASS);
-        assertConfig(config);
+        assertConfig(config, true);
     }
+    
+    
 
-    private void assertConfig(Configuration config)
+    private void assertConfig(Configuration config, boolean isUsingDefaultImports)
     {
         assertEquals(1, config.getEventTypeAliases().size());
         assertEquals(SAMPLE_EVENT_CLASS, config.getEventTypeAliases().get(SAMPLE_EVENT_NAME));
+
+        if(isUsingDefaultImports)
+        {
+            assertEquals(4, config.getImports().size());
+            assertEquals("java.lang.*", config.getImports().get(0));
+            assertEquals("java.math.*", config.getImports().get(1));
+            assertEquals("java.text.*", config.getImports().get(2));
+            assertEquals("java.util.*", config.getImports().get(3));
+        }
+        else
+        {
+        	assertEquals(1, config.getImports().size());
+        	assertEquals(SAMPLE_IMPORT_PACKAGE, config.getImports().get(0));
+        }
     }
 }
