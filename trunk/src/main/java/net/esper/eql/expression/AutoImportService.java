8c6cbfc9d5eedab9e66d6df7c42b7ac3fc59ea3a
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-07-12 16:27:37 +0000

    Removed getImports() and setImports() from the interface.
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/staticmethods@99 d91eb1b3-1215-0410-9718-d41490442091

--- trunk/src/main/java/net/esper/eql/expression/AutoImportService.java
+++ trunk/src/main/java/net/esper/eql/expression/AutoImportService.java
@@ -4,19 +4,6 @@ public interface AutoImportService
 {
 
 	/**
-	 * Get the imports in the same order in which they are attempted.
-	 * @return an array containing the imports in the order
-	 * they will be attempted in resolving a class name
-	 */
-	public String[] getImports();
-
-	/**
-	 * Set the imports. The imports will be searched in the same order as given.
-	 * @param imports - the package and class names that will be used to resolve partial class names
-	 */
-	public void setImports(String[] imports);
-
-	/**
 	 * Gets the Class object for the class name. If the class 
 	 * name is incomplete, uses the imported class and package 
 	 * names to attempt to resolve the name.
