60f7738e86a2eb8fa64b2996514625aedb3c697c
citrucel <citrucel@d91eb1b3-1215-0410-9718-d41490442091>
2006-08-04 00:24:35 +0000

    20060803_First Check In
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/ifthenelsecase@173 d91eb1b3-1215-0410-9718-d41490442091

---
+++ trunk/src/main/java/net/esper/eql/expression/ExprElseNode.java
@@ -0,0 +1,11 @@
+package net.esper.eql.expression;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: MYSELF
+ * Date: Aug 3, 2006
+ * Time: 8:23:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExprElseNode {
+}
