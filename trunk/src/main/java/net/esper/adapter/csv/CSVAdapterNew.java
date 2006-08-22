0ada825911188b3e4ed2ae8be1bb1fcb45c49b86
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-08-22 14:36:21 +0000

    Initial version.
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/csvadapter@283 d91eb1b3-1215-0410-9718-d41490442091

---
+++ trunk/src/main/java/net/esper/adapter/csv/CSVAdapterNew.java
@@ -0,0 +1,29 @@
+package net.esper.adapter.csv;
+
+import net.esper.event.EventAdapterService;
+import net.esper.schedule.ScheduleBucket;
+import net.esper.schedule.SchedulingService;
+
+public class CSVAdapterNew
+{
+	private final EventAdapterService eventAdapterService;
+	private final SchedulingService schedulingService;
+	private final ScheduleBucket scheduleBucket;
+	
+	
+	/**
+	 * Ctor.
+	 * @param eventAdapterService - used for resolving event type aliases
+	 * @param schedulingService - used for making callbacks
+	 * @param scheduleBucket - the scheduling bucket that all adapters use
+	 */
+	public CSVAdapterNew(final EventAdapterService eventAdapterService, final SchedulingService schedulingService, final ScheduleBucket scheduleBucket)
+	{
+		this.eventAdapterService = eventAdapterService;
+		this.schedulingService = schedulingService;
+		this.scheduleBucket = scheduleBucket;
+	}
+	
+	
+	
+}
