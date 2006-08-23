fcd0757f0653debfa3f478c7b3178e4bed3799b2
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-08-23 23:58:08 +0000

    Renamed from TestCSVAdapter.
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/csvadapter@317 d91eb1b3-1215-0410-9718-d41490442091

---
+++ trunk/src/test/java/net/esper/adapter/csv/TestCSVPlayer.java
@@ -0,0 +1,38 @@
+package net.esper.adapter.csv;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+public class TestCSVPlayer extends TestCase
+{
+	private Map<String, Class> propertyTypes;
+	protected void setUp() throws ClassNotFoundException
+	{
+		propertyTypes = new LinkedHashMap<String, Class>();
+		propertyTypes.put("myInt", Integer.class);
+		propertyTypes.put("myDouble", Double.class);
+		propertyTypes.put("myString", String.class);
+	}
+
+	public void testResolveTitleRow()
+	{
+		// Use first row
+		String[] firstRow = new String[] { "myDouble", "myInt", "myString" };
+		assertEquals(firstRow, CSVPlayer.resolvePropertyOrder(firstRow, propertyTypes));
+		
+		firstRow = new String[] { "myDouble", "myInt", "myString", "timestamp" };
+		assertEquals(firstRow, CSVPlayer.resolvePropertyOrder(firstRow, propertyTypes));
+		
+		// Use propertyTypes
+		firstRow = new String[] { "1", "2.0", "text" };
+		String[] expected = propertyTypes.keySet().toArray(new String[0]);
+		assertEquals(Arrays.asList(expected), Arrays.asList(CSVPlayer.resolvePropertyOrder(firstRow, propertyTypes)));
+
+		propertyTypes.put("timestamp", long.class);
+		expected = propertyTypes.keySet().toArray(new String[0]);
+		assertEquals(Arrays.asList(expected), Arrays.asList(CSVPlayer.resolvePropertyOrder(firstRow, propertyTypes)));
+	}
+}
