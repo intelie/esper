fca5aef8891b52c8334a1090257f46382860ac7e
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-08-30 04:02:13 +0000

    Now exposes the send time and schedule slot.
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/csvadapter@338 d91eb1b3-1215-0410-9718-d41490442091

--- trunk/src/main/java/net/esper/adapter/SendableEvent.java
+++ trunk/src/main/java/net/esper/adapter/SendableEvent.java
@@ -1,10 +1,13 @@
 package net.esper.adapter;
 
 import net.esper.client.EPRuntime;
+import net.esper.schedule.ScheduleSlot;
 
 /**
- * An event packaged up so that it can immediately be sent into 
- * the caller-specified runtime.
+ * A wrapper that packages an event up so that it can be 
+ * sent into the caller-specified runtime. It also provides 
+ * the scheduling information for this event (send time and 
+ * schedule slot), so the user can send this event on schedule.
  */
 public interface SendableEvent
 {
@@ -13,4 +16,16 @@ public interface SendableEvent
 	 * @param runtime - the runtime to send the event into
 	 */
 	public void send(EPRuntime runtime);
+	
+	/**
+	 * Get the send time of this event, relative to all the other events sent or read by the same entity
+	 * @return timestamp
+	 */
+	public long getSendTime();
+	
+	/**
+	 * Get the schedule slot for the entity that created this event
+	 * @return schedule slot
+	 */
+	public ScheduleSlot getScheduleSlot();
 }
