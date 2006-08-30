5926cb6b67b304376c010dd4b80fba2288af26e3
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-08-30 04:07:55 +0000

    Support for schedule information.
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/csvadapter@344 d91eb1b3-1215-0410-9718-d41490442091

--- trunk/src/main/java/net/esper/adapter/csv/SendableMapEvent.java
+++ trunk/src/main/java/net/esper/adapter/csv/SendableMapEvent.java
@@ -5,30 +5,51 @@ import java.util.Map;
 
 import net.esper.adapter.SendableEvent;
 import net.esper.client.EPRuntime;
+import net.esper.schedule.ScheduleSlot;
 
 /**
- * An instance of SendableEvent that wraps a Map event for
+ * An implementation of SendableEvent that wraps a Map event for
  * sending into the runtime.
  */
 public class SendableMapEvent implements SendableEvent
 {
 	private final Map<String, Object> mapToSend;
 	private final String eventTypeAlias;
+	private final long timestamp;
+	private final ScheduleSlot scheduleSlot;
 	
 	/**
 	 * Ctor.
 	 * @param mapToSend - the map to send into the runtime
 	 * @param eventTypeAlias - the event type alias for the map event
+	 * @param timestamp - the timestamp for this event
+	 * @param scheduleSlot - the schedule slot for the entity that created this event
 	 */
-	public SendableMapEvent(Map<String, Object> mapToSend, String eventTypeAlias)
+	public SendableMapEvent(Map<String, Object> mapToSend, String eventTypeAlias, long timestamp, ScheduleSlot scheduleSlot)
 	{
 		this.mapToSend = new HashMap<String, Object>(mapToSend);
 		this.eventTypeAlias = eventTypeAlias;
+		this.timestamp = timestamp;
+		this.scheduleSlot = scheduleSlot;
 	}
 	
 	public void send(EPRuntime runtime)
 	{
 		runtime.sendEvent(mapToSend, eventTypeAlias);
 	}
+	
+	public ScheduleSlot getScheduleSlot()
+	{
+		return scheduleSlot;
+	}
 
+	public long getSendTime()
+	{
+		return timestamp;
+	}
+	
+	public String toString()
+	{
+		return mapToSend.toString();
+	}
 }
