5870e9caed482e717a371f8e09bb01c7ba58f52f
bernhardttom <bernhardttom@d91eb1b3-1215-0410-9718-d41490442091>
2006-12-27 02:09:26 +0000

    deleted old branch
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/enhancements120@572 d91eb1b3-1215-0410-9718-d41490442091

--- trunk/examples/src/main/java/net/esper/example/transaction/TxnEventB.java
+++
@@ -1,9 +0,0 @@
-package net.esper.example.transaction;
-
-public class TxnEventB extends TxnEventBase
-{
-    public TxnEventB(String transactionId, long timestamp)
-    {
-        super(transactionId, timestamp);
-    }
-}
--- trunk/examples/terminalsvc/terminalsvc-common/src/main/java/net/esper/example/terminal/common/OutOfOrder.java
+++
@@ -1,9 +0,0 @@
-package net.esper.example.terminal.common;
-
-public class OutOfOrder extends BaseTerminalEvent
-{
-    public OutOfOrder(TerminalInfo deskInfo)
-    {
-        super(deskInfo);
-    }
-}
