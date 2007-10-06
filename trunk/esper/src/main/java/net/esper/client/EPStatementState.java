bd8c6f2c33cf661fc3ac62db977170bf8cf078bb
bernhardttom <bernhardttom@d91eb1b3-1215-0410-9718-d41490442091>
2007-10-06 17:17:50 +0000

    Added some missing copyright headers
    git-svn-id: http://svn.codehaus.org/esper/esper/branches/enhancements111@988 d91eb1b3-1215-0410-9718-d41490442091

--- trunk/esper/src/main/java/net/esper/client/EPStatementState.java
+++ trunk/esper/src/main/java/net/esper/client/EPStatementState.java
@@ -1,3 +1,10 @@
+/**************************************************************************************
+ * Copyright (C) 2007 Thomas Bernhardt. All rights reserved.                          *
+ * http://esper.codehaus.org                                                          *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the GPL license       *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
 package net.esper.client;
 
 /**
